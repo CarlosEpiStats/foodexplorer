50e01561f8c643a7e883a8fd4993d4b698222421
CarlosEpiStats <carlos.fernandez4.ext@bayer.com>
2025-12-02 18:05:04 +0100

    Mock shinydashboard page

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,7 +10,13 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
-      golem::golem_welcome_page() # Remove this line to start building your UI
+      shinydashboard::dashboardPage(
+        header = shinydashboard::dashboardHeader(
+          title = "foodexplorer Shiny App"
+        ),
+        sidebar = shinydashboard::dashboardSidebar(),
+        body = shinydashboard::dashboardBody()
+      )
     )
   )
 }
