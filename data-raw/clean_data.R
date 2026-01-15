63adeda108f2c0a9724e35130ee18578d9a49af8
CarlosEpiStats <carlos.fernandez4.ext@bayer.com>
2026-01-15 17:27:29 +0100

    Finished cleaning hierarchy.
    Next: try with whole worksheet

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -28,6 +28,15 @@ get_hierarchy_level <- function(data, level) {
     dplyr::select("row", "col", {{ variable_name }} := "product")
 }
 
+# Clean variables
+clean_names <- function(variable) {
+  variable <- plyr::mapvalues(
+    variable,
+    from = unique(variable),
+    to = janitor::make_clean_names(unique(variable))
+  )
+  variable
+}
 ### Transform Excel cells ----
 clean_file <- function(file_path) {
   # Loop for sll sheets
@@ -54,14 +63,12 @@ clean_file <- function(file_path) {
       unpivotr::behead(direction = "NNW", name = "region") |> # finds the region as a header direction north-west (up, then left)
       unpivotr::behead(direction = "N", name = "variable") |> # the variable is right above the cell value (north: up)
       unpivotr::enhead(cells_products, direction = "left") |> # adds back the product name
-      # unpivotr::enhead(cells_hierarchy, direction = "left") |> # adds back the hierarchy level
       dplyr::select(
         "row",
         "col",
         "variable",
         "region",
         "product",
-        # "hierarchy",
         "numeric"
       )
 
@@ -75,32 +82,22 @@ clean_file <- function(file_path) {
     )
     data_list[[sheet_index]] <- values
   }
-  data <- dplyr::bind_rows(data_list)
+  data <- dplyr::bind_rows(data_list) |>
+    # Clean names
+    dplyr::mutate(
+      dplyr::across(
+        tidyselect::all_of(c("product", "variable", "region")),
+        clean_names
+      )
+    )
 
-  #Clean names
-  products_names_raw <- unique(data$product)
-  products_names_clean <- janitor::make_clean_names(products_names_raw)
-  data$product <- plyr::mapvalues(
-    data$product,
-    from = products_names_raw,
-    to = products_names_clean
-  )
-  variable_names_raw <- unique(data$variable)
-  variable_names_clean <- janitor::make_clean_names(variable_names_raw)
-  data$variable <- plyr::mapvalues(
-    data$variable,
-    from = variable_names_raw,
-    to = variable_names_clean
-  )
-  region_names_raw <- unique(data$region)
-  region_names_clean <- janitor::make_clean_names(region_names_raw)
-  data$region <- plyr::mapvalues(
-    data$region,
-    from = region_names_raw,
-    to = region_names_clean
-  )
+  # Obtain nested hierarchy
+  hierarchy <- get_hierarchy_table(file_path)
+  data_hierarchy <- data |>
+    dplyr::left_join(hierarchy, by = c("product", "row"))
+  message("Hierarchy added to ", file_name)
   message("Finished cleaning file: ", file_name, " -----------------------")
-  data
+  data_hierarchy
 }
 
 # Use the first sheet as template to extract the hierarchies
@@ -108,14 +105,17 @@ get_hierarchy_table <- function(file_path) {
   # Get personalized Excel formats, that convey meaning of hierarchy
   formats <- tidyxl::xlsx_formats(file_path)
   indent <- formats$local$alignment$indent # Maps each local_format_id to an indentation number
+  sheet_names <- readxl::excel_sheets(file_path)
   cells <- extract_cells(file_path, sheet_names[1]) |>
     dplyr::select("row", "col", product = "character", "local_format_id")
 
   # Get each product and its hierarchy
   products_hierarchy <- cells |>
     dplyr::filter(.data$col == 1, .data$row >= 4) |>
-    dplyr::mutate(hierarchy = indent[.data$local_format_id]) # 0 is at the top, then 1, etc.
-
+    dplyr::mutate(
+      hierarchy = indent[.data$local_format_id], # 0 is at the top, then 1, etc.
+      product = clean_names(.data$product)
+    )
   hierarchy_0 <- get_hierarchy_level(products_hierarchy, 0)
   hierarchy_1 <- get_hierarchy_level(products_hierarchy, 1)
   hierarchy_2 <- get_hierarchy_level(products_hierarchy, 2)
@@ -161,7 +161,9 @@ get_hierarchy_table <- function(file_path) {
     products_4,
     products_5
   ) |>
-    dplyr::select(-c("row", "col", "local_format_id"))
+    dplyr::select(-c("col", "local_format_id"))
+  # There are duplicated names in different subcategories, so we keep row as identifier
+  # Example, three rows for PIMIENTOS (in T.HORTALIZAS FRESCAS, FRUTA&HORTA.CONSERVA, FRUTA&HORTA.CONGELAD)
   data_hierarchy
 }
 
@@ -173,4 +175,4 @@ file_name <- file_list[29]
 file_path <- here::here(file_dir, file_name)
 
 test_data <- clean_file(file_path)
-get_hierarchy_table(file_path)
+(hierarchy <- get_hierarchy_table(file_path))
